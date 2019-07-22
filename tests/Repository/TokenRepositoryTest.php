38fd8755c4b91e46066fb75447247b7b9bb1ec8f
Santa <santa.ringostat@gmail.com>
2019-07-22 17:48:51 +0300

    initialize test DB

--- tests/Repository/TokenRepositoryTest.php
+++ tests/Repository/TokenRepositoryTest.php
@@ -11,24 +11,41 @@ namespace Tests\App\Repository;
 use App\Entity\Token;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\StringInput;
 
 class TokenRepositoryTest extends KernelTestCase
 {
     private $em;
     private $user;
+    private $application;
 
     protected function setUp():void
     {
+        $kernel = static::createKernel();
+        $this->application = new Application($kernel);
+        $this->application->setAutoExit(false);
+        $this->runCommand('doctrine:database:drop --force');
+        $this->runCommand('doctrine:database:create');
+        $this->runCommand('doctrine:schema:create');
+//        $this->runCommand('doctrine:fixtures:load --append --no-interaction');
+        
         $kernel = self::bootKernel();
 
         $this->em = $kernel->getContainer()->get('doctrine')->getManager();
-
         $this->user = new User('test@gmail.com', 'test', []);
 
         $this->em->persist($this->user);
         $this->em->flush();
     }
 
+
+    protected function runCommand($command)
+    {
+//        $command = sprintf('%s ', $command);
+        return $this->application->run(new StringInput($command));
+    }
+    
     public function testCreateNewFor()
     {
         $user = $this->em->getRepository(User::class)->findOneBy(['id' => $this->user->getId()]);
@@ -47,6 +64,7 @@ class TokenRepositoryTest extends KernelTestCase
 
         $this->em->close();
         $this->em= null;
+        $this->runCommand('doctrine:database:drop --force');
     }
 
 }
